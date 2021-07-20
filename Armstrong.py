a861ae76cd787babe289c07b0aef401cf52aa48b
Jyoti Anant Dipke <Jyotidipke@gmail.com>
2021-07-20 23:04:19 +0530

    Add files via upload

---
+++ Armstrong.py
@@ -0,0 +1,11 @@
+#armstrong 
+##n=int(input('Enter number'))
+##m=n
+##p=len(str(n))
+##ans=0
+##while n>0:
+##    mod=n%10
+##    ans=ans+mod**p
+##    n=n//10
+##print('Armstrong' if m==ans else 'Not Armstrong')  
+
